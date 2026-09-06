8b4695956d95a99a8c110e56903342d7455299b9
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-06 16:25:50 +0800

    建立資料庫與建立任務

---
+++ db/migrate/20260904131722_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[8.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false # 標題
+      t.text :content # 內文
+      t.string :status,default: "pending", null: false #狀態
+
+      # 時間戳記
+      t.timestamps
+    end
+  end
+end
