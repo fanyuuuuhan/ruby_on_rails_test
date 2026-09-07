6af5cc33cd4fa6d30169c3f02f1a5cb3fbea2d08
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-07 11:21:10 +0800

    建立資料庫與建立任務-修正AI

--- db/migrate/20260904131722_create_tasks.rb
+++ db/migrate/20260904131722_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[8.1]
     create_table :tasks do |t|
       t.string :title, null: false # 標題
       t.text :content # 內文
-      t.string :status,default: "pending", null: false #狀態
+      t.string :status, default: "pending", null: false # 狀態
 
       # 時間戳記
       t.timestamps
