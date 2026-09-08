59e9da69a22a8a5a04c8c5870c5b5b04458cb7a1
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-09 01:09:01 +0800

    新增、修改、檢視、刪除任務

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,5 +1,16 @@
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  status     :string           default("pending"), not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
