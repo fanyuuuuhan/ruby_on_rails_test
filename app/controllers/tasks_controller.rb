1228f0f57a43f2aa7b212bb2618e59fa5b81292f
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-09 10:45:12 +0800

    新增、修改、檢視、刪除任務-修正CI

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-
   # 在顯示、編輯、更新和刪除操作之前，先設定好 @task
-  #避免重複的程式碼
+  # 避免重複的程式碼
   before_action :set_task, only: %i[show edit update destroy]
 
 
@@ -12,47 +11,47 @@ class TasksController < ApplicationController
   def show
   end
 
-  #顯示新增任務的表單
+  # 顯示新增任務的表單
   def new
     @task = Task.new
   end
 
-  #建立任務
-  #失敗回到new
+  # 建立任務
+  # 失敗回到new
   def create
     @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_path, notice: "任務建立成功"
     else
       flash.now[:alert] = "任務建立失敗"
-      render :new , status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
-  #更新任務
-  #成功->詳細頁面，
-  #失敗->回到編輯頁面
+  # 更新任務
+  # 成功->詳細頁面，
+  # 失敗->回到編輯頁面
   def update
     if @task.update(task_params)
       redirect_to @task, notice: "任務更新成功"
     else
       flash.now[:alert] = "任務更新失敗"
-      render :edit , status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  #刪除任務
+  # 刪除任務
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: "任務刪除成功"
   end
 
-  #編輯任務
+  # 編輯任務
   def edit
   end
 
   private
-   #僅允許 title, content, status 這三個欄位被傳入
+  # 僅允許 title, content, status 這三個欄位被傳入
   def task_params
     params.require(:task).permit(:title, :content, :status)
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,6 @@
 #  updated_at :datetime         not null
 #
 class Task < ApplicationRecord
-    validates :title, presence: true, length: { maximum: 100 } 
+    validates :title, presence: true, length: { maximum: 100 }
     validates :status, presence: true
 end
