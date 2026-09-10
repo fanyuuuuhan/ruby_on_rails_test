16bd5d9d3635e365c6996cbaf95439ebe2206607
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-10 15:16:44 +0800

    indent設置統一 修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,19 +10,21 @@
 #  updated_at :datetime         not null
 #
 class Task < ApplicationRecord
-        validates :title, presence: true,
+  validates :title, presence: true,
             length: { maximum: 100 },
             uniqueness: {
-                case_sensitive: false,
-                message: "此標題已經存在"
+              case_sensitive: false,
+              message: "此標題已經存在"
             }
-        validates :content, length: {
-            maximum: 1000,
-            message: "不能超過1000個字"
-        }, allow_blank: true
-        validates :status, presence: true,
+  validates :content, 
+            length: {
+              maximum: 1000,
+              message: "不能超過1000個字"
+            }
+            allow_blank: true
+  validates :status, presence: true,
             inclusion: {
-                in: %w[pending in_progress completed],
-                message: "不是有效的狀態"
+              in: %w[pending in_progress completed],
+              message: "不是有效的狀態"
             }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_10_040209) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_04_131722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,8 +20,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_040209) do
     t.string "status", default: "pending", null: false
     t.string "title", null: false
     t.datetime "updated_at", null: false
-    t.index "lower(TRIM(BOTH FROM title))", name: "unique_task_title", unique: true
-    t.check_constraint "char_length(content) <= 1000", name: "task_content_length_check"
-    t.check_constraint "status::text = ANY (ARRAY['pending'::character varying, 'in_progress'::character varying, 'completed'::character varying]::text[])", name: "task_status_check"
   end
 end
