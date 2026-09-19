a69c9e43c3b229f0e470ea42585b2bf73aadb599
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2026-09-19 15:08:46 +0800

    CI

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,13 +2,13 @@
 module TasksHelper
   def task_options_for_select
     [
-        [t("依建立時間排序(由舊到新)"), "created_at_asc"],
-        [t("依建立時間排序(由新到舊)"), "created_at_desc"],
-        [t("依標題排序(由小到大)"), "title_asc"],
-        [t("依截止日期排序(由近到遠)"), "due_date_asc"],
-        [t("依截止日期排序(由遠到近)"), "due_date_desc"],
-        [t("依優先順序排序(由低到高)"), "priority_asc"],
-        [t("依優先順序排序(由高到低)"), "priority_desc"]
-
+        [ t("tasks.sort_orders.created_at_asc"), "created_at_asc" ],
+        [ t("tasks.sort_orders.created_at_desc"), "created_at_desc" ],
+        [ t("tasks.sort_orders.title_asc"), "title_asc" ],
+        [ t("tasks.sort_orders.due_date_asc"), "due_date_asc" ],
+        [ t("tasks.sort_orders.due_date_desc"), "due_date_desc" ],
+        [ t("tasks.sort_orders.priority_asc"), "priority_asc" ],
+        [ t("tasks.sort_orders.priority_desc"), "priority_desc" ]
     ]
-    
+  end
+end
